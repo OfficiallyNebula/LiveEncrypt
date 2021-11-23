79bf4269419cf27e1b5250169c0371d44b5863ee
Ben Harris <56564007+OfficiallyNebula@users.noreply.github.com>
2021-11-23 08:29:09 +0000

    Moved class from main Program.CS
    Encryption is now its own class so it can be used stand-alone without the overhead of the entire project.
    (this gives users the option to encrypt a string but not have the ability to decrypt it)

---
+++ Encryption.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveEncrypt
+{
+    class Encryption
+    {
+        public void Encrypt(string input)
+        {
+
+            //buzz
+            //foobbar
+            //fooubar
+            //foozbar
+            //foozbar
+            //foobbarfooubarfoozbarfoozbar
+            string key1 = "foo";
+            string key2 = "bar";
+
+            //Format Date to string and get rid of date to leave time. (HH is 24 hour, hh is 12 hour)
+            var timeString = DateTime.Now.ToString("HH:mm:ss");
+
+            using (StreamWriter writer = new StreamWriter("Encryption.neb", append: true))
+            {
+
+                writer.Write($"{timeString}");
+            }
+            foreach (char c in input.ToCharArray())
+            {
+                try
+                {
+                    string combine = key1 + c + key2;
+                    Console.WriteLine(combine);
+                    using (StreamWriter writer = new StreamWriter("Encryption.neb", append: true))
+                    {
+                        writer.Write($"{combine.ToString().ToLower().Trim()}");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(@"Error within Try/Catch of Encryption");
+                    throw;
+                }
+
+            }
+            using (StreamWriter writer = new StreamWriter("Encryption.neb", append: true))
+            {
+                writer.WriteLine();
+            }
+        }
+    }
+}
