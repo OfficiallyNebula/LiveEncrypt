20f419807e4fc31e934fb406228ebdf25c45c453
Ben Harris <56564007+OfficiallyNebula@users.noreply.github.com>
2021-11-23 09:18:54 +0000

    Initial Commit of MessageHandler Class
    Bare Bones commit of the MessageHandler class.

---
+++ MessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveEncrypt
+{
+    class MessageHandler
+    {
+        //Create method that takes a string input and has an optional delay to be passed debug mode is disabled by default - message handler will not handle debugs.
+        public void Message(string input, int delay = 0, bool debug = false)
+        {
+            if (!debug)
+            {
+                Console.WriteLine($"MH >> {input}");
+                //Some stuff here to run whenever debug is not enabled (simple messaging)
+            } else
+            {
+                //If debug enabled, run this.
+            }
+
+
+
+
+        }
+
+    }
+}
