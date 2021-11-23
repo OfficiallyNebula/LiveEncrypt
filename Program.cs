46be504f7d461f3761ca5c1c7d9823722b7fabdb
Ben Harris <56564007+OfficiallyNebula@users.noreply.github.com>
2021-11-23 12:20:40 +0000

    Created basic menu
    Menu navigates user to either enter a phrase and return their MD5 Sum, or check if two phrases have the same MD5 Sum.

--- Program.cs
+++ Program.cs
@@ -8,12 +8,64 @@ namespace LiveEncrypt
 
         public static void Main(string[] args)
         {
-            Encryption encryption = new Encryption();
             MessageHandler mh = new MessageHandler();
 
-            mh.Message("Please enter your desired string");
-            string input = mh.Input();
-            mh.Message(encryption.CreateMD5(input));
+            while (true)
+            {
+                mh.Message("-----[Live Encrypt]-----");
+                mh.Message("Please choose one of the following options");
+                mh.Message("");
+                mh.Message("1) Create MD5 Sum of input");
+                mh.Message("2) Check MD5 Sum matches");
+                int choice = 0;
+
+                while(true)
+                {
+                    try
+                    {
+                        choice = Convert.ToInt32(mh.Input());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        mh.Message($"Exception thrown when converting string to int32 - Exception code: {e}");
+                        throw;
+                    }
+                }
+                if (choice == 1)
+                {
+                    Encryption encryption = new Encryption();
+                    mh.Message("Please enter the phrase you wish to encrypt.");
+                    string input = mh.Input();
+                    mh.Message($"Your MD5 Hash is: {encryption.CreateMD5(input)}");
+                    break;
+                }
+                else if (choice == 2)
+                {
+                    Encryption encryption = new Encryption();
+                    mh.Message("Please enter the first phrase you wish to encrypt.");
+                    string answer1 = mh.Input();
+                    string md5result1 = encryption.CreateMD5(answer1);
+                    mh.Message("Please enter the second phrase you wish to encrypt.");
+                    string answer2 = mh.Input();
+                    string md5result2 = encryption.CreateMD5(answer2);
+
+                    if (md5result1 == md5result2)
+                    {
+                        mh.Message("Your MD5 Hash's are the same.");
+                        mh.Message($"Hash 1 = {md5result1}");
+                        mh.Message($"Hash 2 = {md5result2}");
+                        mh.LineBreak();
+                    } else
+                    {
+                        mh.Message("Your MD5 Hash's are NOT same.");
+                        mh.Message($"Hash 1 = {md5result1}");
+                        mh.Message($"Hash 2 = {md5result2}");
+                        mh.LineBreak();
+                    }
+
+                }
+            }
         }
 
     }
